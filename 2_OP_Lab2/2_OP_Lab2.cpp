4f51e8cf6f8c0e8fa8d25d0ce46b3dd1feb1e621
maks-alex <65873569+maks-alex@users.noreply.github.com>
2020-06-09 00:43:12 +0300

    Dijkstra from adjacency matrix

--- 2_OP_Lab2/2_OP_Lab2.cpp
+++ 2_OP_Lab2/2_OP_Lab2.cpp
@@ -1,20 +1,114 @@
-// 2_OP_Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 6
 int main()
 {
-    std::cout << "Hello World!\n";
+    //   int a[SIZE][SIZE]; // матрица связей
+    int d[SIZE]; // минимальное расстояние
+    int v[SIZE]; // посещенные вершины
+    int temp, minindex, min;
+    int begin_index = 0;
+    system("chcp 1251");
+    system("cls");
+    // Инициализация матрицы связей
+    /*
+    for (int i = 0; i < SIZE; i++)
+    {
+        a[i][i] = 0;
+        for (int j = i + 1; j < SIZE; j++) {
+            printf("Введите расстояние %d - %d: ", i + 1, j + 1);
+            scanf("%d", &temp);
+            a[i][j] = temp;
+            a[j][i] = temp;
+        }
+    }
+    */
+    int a[6][6] =
+    {
+        {0,7,9,0,0,14},
+        {7,0,10,15,0,0},
+        {9,10,0,11,0,2},
+        {0,15,11,0,6,0},
+        {0,0,0,6,0,9},
+        {14,0,2,0,9,0},
+    };
+
+    // Вывод матрицы связей
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+            printf("%5d ", a[i][j]);
+        printf("\n");
+    }
+    //Инициализация вершин и расстояний
+    for (int i = 0; i < SIZE; i++)
+    {
+        d[i] = 10000;
+        v[i] = 1;
+    }
+    d[begin_index] = 0;
+    // Шаг алгоритма
+    do {
+        minindex = 10000;
+        min = 10000;
+        for (int i = 0; i < SIZE; i++)
+        { // Если вершину ещё не обошли и вес меньше min
+            if ((v[i] == 1) && (d[i] < min))
+            { // Переприсваиваем значения
+                min = d[i];
+                minindex = i;
+            }
+        }
+        // Добавляем найденный минимальный вес
+        // к текущему весу вершины
+        // и сравниваем с текущим минимальным весом вершины
+        if (minindex != 10000)
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                if (a[minindex][i] > 0)
+                {
+                    temp = min + a[minindex][i];
+                    if (temp < d[i])
+                    {
+                        d[i] = temp;
+                    }
+                }
+            }
+            v[minindex] = 0;
+        }
+    } while (minindex < 10000);
+    // Вывод кратчайших расстояний до вершин
+    printf("\nКратчайшие расстояния до вершин: \n");
+    for (int i = 0; i < SIZE; i++)
+        printf("%5d ", d[i]);
+
+    // Восстановление пути
+    int ver[SIZE]; // массив посещенных вершин
+    int end = 4; // индекс конечной вершины = 5 - 1
+    ver[0] = end + 1; // начальный элемент - конечная вершина
+    int k = 1; // индекс предыдущей вершины
+    int weight = d[end]; // вес конечной вершины
+
+    while (end != begin_index) // пока не дошли до начальной вершины
+    {
+        for (int i = 0; i < SIZE; i++) // просматриваем все вершины
+            if (a[i][end] != 0)   // если связь есть
+            {
+                int temp = weight - a[i][end]; // определяем вес пути из предыдущей вершины
+                if (temp == d[i]) // если вес совпал с рассчитанным
+                {                 // значит из этой вершины и был переход
+                    weight = temp; // сохраняем новый вес
+                    end = i;       // сохраняем предыдущую вершину
+                    ver[k] = i + 1; // и записываем ее в массив
+                    k++;
+                }
+            }
+    }
+    // Вывод пути (начальная вершина оказалась в конце массива из k элементов)
+    printf("\nВывод кратчайшего пути\n");
+    for (int i = k - 1; i >= 0; i--)
+        printf("%3d ", ver[i]);
+    getchar(); getchar();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
